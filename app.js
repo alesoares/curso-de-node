120616f7495847bb1d585039e008725365be4cb3
Alessandro Soares. <alesoares1975@gmail.com>
2024-09-28 23:31:53 -0300

    I created several folders and their files, installed Express, NPM and EJS

---
+++ app.js
@@ -0,0 +1,43 @@
+
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res){
+    res.render('home/index');
+});
+
+app.get('/formulario_inclusao_noticia', function(req, res){
+    res.render('admin/form_add_noticia');
+});
+
+app.get('/noticias', function(req, res){
+    res.render('noticias/noticias');
+});
+
+app.get('/tecnologia', function(req, res){
+    res.render('secao/tecnologia');
+});
+
+app.get('/tecnologia', function(req, res){
+    res.render('secao/tecnologia');
+});
+
+
+
+
+
+
+
+app.get('/', function(req, res){
+    res.send('<html><body>Bem vindo ao meu site!!!</body></html>');
+});
+
+app.get('/sobre', function(req, res){
+    res.send('<html><body>Sobre mim</body></html>');
+});
+
+app.listen(3000, function(){
+    console.log("Servidor rodando com Express");
+});
