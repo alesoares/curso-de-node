b96f3e0f4de1d79ff71811e5c54cc24937c52383
Alessandro Soares. <alesoares1975@gmail.com>
2024-09-29 19:16:16 -0300

    I modified the app.js file, renamed the routes as well, and modified each route file in the repository.

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,5 +1,6 @@
-module.exports = function(app){
-    app.get('/noticias', function(req,res){
+
+module.exports = function (app) {
+    app.get('/noticias', function (req, res) {
         res.render('noticias/noticias');
     });
 }
